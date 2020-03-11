7a5670127e784797260ed19d7709b86bca53a7f2
cetdo <cetdo@outlook.com>
2020-03-11 10:32:03 -0400

    Projeto em branco no Sphinx com o sphinx-quickstart

---
+++ _build/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{"\u00edndice":0,"m\u00f3dulo":0,"p\u00e1gina":0,pesquisa:0},titles:["Welcome to Esfinge\u2019s documentation!"],titleterms:{document:0,esfing:0,indic:0,tabl:0,welcom:0}})
